5ca2be6ef0dcf6a872b20ffc81642136f0fb822a
HowlTheHusky <32062494+HowlTheHusky@users.noreply.github.com>
2017-09-18 19:13:19 +0200

    Mover.h Patch

--- _Common/Mover.h
+++ _Common/Mover.h
@@ -942,6 +942,12 @@ public:
 	static BOOL		DoEquip( int nSex, int nSkinSet, CItemElem* pItemElem, int nPart, const EQUIP_INFO & rEquipInfo, CItemContainer< CItemElem  >* pInventory, PEQUIP_INFO pEquipeInfo, CModelObject* pModel, BOOL bEquip, CMover *pMover );
 	static	float	GetItemEnduranceInfluence( int nEndurance );	
 	static	int		GetItemEnduranceWeight( int nEndurance );	
+	
+#ifdef __EXTENDED_CURRENCY
+	int						GetChipFarmNum();
+	int						GetChipLgNum();
+	int						GetChipDonateNum();
+#endif // __EXTENDED_CURRENCY
 
 	virtual	BOOL	SetIndex( LPDIRECT3DDEVICE9 pd3dDevice, DWORD dwIndex, BOOL bInitProp = FALSE, BOOL bDestParam = TRUE );
 	virtual BOOL	Read( CFileIO* pFile );
